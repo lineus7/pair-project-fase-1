f9b28fc24fe8e7307b4587f0950a93b30b28c4ae
Arezafaza16 <arezafaza@gmail.com>
2023-12-22 09:07:08 +0700

    hahaha

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -2,22 +2,22 @@ const { Op } = require("sequelize")
 const { User, Doctor, Patient, Chat, Medicine, PatientMedicine } = require(`../models`)
 const rupiah = require("../helper/formatRupiah")
 class PatientController {
-    static async showListDoctor(req,res){
+    static async showListDoctor(req, res) {
         try {
-            let {filter,search} = req.query
-            let option = {order:[[`id`,`ASC`]]}
-            if (filter) option.where = {specialist:filter}
-            if (search) option.where = {name:{[Op.iLike]: `%${search}%`}}
+            let { filter, search } = req.query
+            let option = { order: [[`id`, `ASC`]] }
+            if (filter) option.where = { specialist: filter }
+            if (search) option.where = { name: { [Op.iLike]: `%${search}%` } }
             let patient = await Patient.findByPk(req.session.user.PatientId)
             let data = await Doctor.findAll(option)
             // console.log(data);
-            res.render(`listDoctor`,{data,patient,rupiah})
+            res.render(`listDoctor`, { data, patient, rupiah })
         } catch (error) {
             res.send(error)
         }
     }
 
-    static async logout(req,res){
+    static async logout(req, res) {
         try {
             delete req.session.user
             res.redirect(`/login`)
@@ -26,67 +26,67 @@ class PatientController {
         }
     }
 
-    static async showMedicine(req,res){
+    static async showMedicine(req, res) {
         try {
-            const {done} = req.query
+            const { done } = req.query
             let data = await Medicine.findAll()
-            res.render(`listMedicine`,{data,done})
+            res.render(`listMedicine`, { data, done, rupiah })
         } catch (error) {
             res.send(error)
         }
     }
 
-    static async buyMedicine(req,res){
+    static async buyMedicine(req, res) {
         try {
-            await PatientMedicine.create({MedicineId:req.params.MedicineId,PatientId:req.session.user.PatientId})
+            await PatientMedicine.create({ MedicineId: req.params.MedicineId, PatientId: req.session.user.PatientId })
             res.redirect(`/patient/medicine?done=Thanks for buying our medicine`)
         } catch (error) {
             res.send(error)
         }
     }
 
-    static async registerDoctorToPatient(req,res){
+    static async registerDoctorToPatient(req, res) {
         try {
             // console.log(req.params.DoctorId,req.session.user);
-            await Patient.update({DoctorId:req.params.DoctorId,status:"Waiting"},{where : {id:req.session.user.PatientId}})
+            await Patient.update({ DoctorId: req.params.DoctorId, status: "Waiting" }, { where: { id: req.session.user.PatientId } })
             res.redirect(`/patient`)
         } catch (error) {
             res.send(error)
         }
     }
 
-    static async cancelConsultation(req,res){
+    static async cancelConsultation(req, res) {
         try {
-            await Chat.create({text:`Percakapan berakhir pada ${new Date()}`,DoctorId:req.params.DoctorId,PatientId:req.session.user.PatientId, from:`System`})
-            await Patient.update({DoctorId:null,status:"Pending"}, {where:{id:req.session.user.PatientId}})
+            await Chat.create({ text: `Percakapan berakhir pada ${new Date()}`, DoctorId: req.params.DoctorId, PatientId: req.session.user.PatientId, from: `System` })
+            await Patient.update({ DoctorId: null, status: "Pending" }, { where: { id: req.session.user.PatientId } })
             res.redirect(`/patient`)
         } catch (error) {
             res.send(error)
         }
     }
 
-    static async showChatFromPatient(req,res){
+    static async showChatFromPatient(req, res) {
         try {
             let doctor = await Doctor.findByPk(req.params.DoctorId)
             let data = await Chat.findAll({
-                where:{[Op.and]: [{ DoctorId: req.params.DoctorId }, { PatientId: req.session.user.PatientId }]},
-                order: [[`createdAt`,`ASC`]]
+                where: { [Op.and]: [{ DoctorId: req.params.DoctorId }, { PatientId: req.session.user.PatientId }] },
+                order: [[`createdAt`, `ASC`]]
             })
-            res.render(`chatFromPatient`,{data,doctor})
+            res.render(`chatFromPatient`, { data, doctor })
         } catch (error) {
             res.send(error)
         }
     }
 
-    static async addChatFromPatient(req,res){
+    static async addChatFromPatient(req, res) {
         try {
-            const {text} = req.body
-            await Chat.create({text,DoctorId:req.params.DoctorId, PatientId: req.session.user.PatientId, from:`Patient`})
+            const { text } = req.body
+            await Chat.create({ text, DoctorId: req.params.DoctorId, PatientId: req.session.user.PatientId, from: `Patient` })
             res.redirect(`/patient/${req.params.DoctorId}/chat`)
         } catch (error) {
             res.send(error)
         }
     }
-    
+
 }
 module.exports = PatientController
